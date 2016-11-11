08c3c6efb8cd7e26ead83735c489ae28159fec31
Ryan Pitts <ryan.a.pitts@gmail.com>
2016-11-11 11:07:56 -0800

    remove custom admin url bit in production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -169,8 +169,3 @@ LOGGING = {
         }
     }
 }
-
-# Custom Admin URL, use {% url 'admin:index' %}
-ADMIN_URL = env('DJANGO_ADMIN_URL')
-
-# Your production stuff: Below this line define 3rd party library settings
